76bb5fd0618219a4ea0d0c9b5eea1f32cfc60c51
wtao <wtao@mux.com>
2018-07-25 13:01:51 -0700

    Cast and control

--- app/app/src/main/java/com/mux/sender/MainActivity.java
+++ app/app/src/main/java/com/mux/sender/MainActivity.java
@@ -1,24 +1,119 @@
 package com.mux.sender;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
+import com.google.android.gms.cast.MediaInfo;
+import com.google.android.gms.cast.MediaLoadOptions;
+import com.google.android.gms.cast.MediaMetadata;
 import com.google.android.gms.cast.framework.CastButtonFactory;
 import com.google.android.gms.cast.framework.CastContext;
+import com.google.android.gms.cast.framework.Session;
+import com.google.android.gms.cast.framework.SessionManager;
+import com.google.android.gms.cast.framework.SessionManagerListener;
+import com.google.android.gms.cast.framework.media.RemoteMediaClient;
+import com.google.android.gms.common.images.WebImage;
 
 public class MainActivity extends AppCompatActivity {
-    CastContext castContext;
+    private CastContext castContext;
+    private SessionManager sessionManager;
+    private SessionManagerListener sessionManagerListener = new SessionManagerListener() {
+
+        @Override
+        public void onSessionStarting(Session session) {
+
+        }
+
+        @Override
+        public void onSessionStarted(Session session, String s) {
+            invalidateOptionsMenu();
+
+            MediaMetadata movieMetadata = new MediaMetadata(MediaMetadata.MEDIA_TYPE_MOVIE);
+
+            movieMetadata.putString(MediaMetadata.KEY_TITLE, "Big Buck Bunny");
+            movieMetadata.putString(MediaMetadata.KEY_SUBTITLE, "The Blender Foundation");
+            movieMetadata.addImage(new WebImage(Uri.parse("https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Big_buck_bunny_poster_big.jpg/220px-Big_buck_bunny_poster_big.jpg")));
+            movieMetadata.addImage(new WebImage(Uri.parse("https://image.tmdb.org/t/p/w533_and_h300_bestv2/aHLST0g8sOE1ixCxRDgM35SKwwp.jpg")));
+
+            MediaInfo mediaInfo = new MediaInfo.Builder("http://184.72.239.149/vod/smil:BigBuckBunny.smil/playlist.m3u8")
+                    .setStreamType(MediaInfo.STREAM_TYPE_BUFFERED)
+                    .setContentType("videos/m3u8")
+                    .setMetadata(movieMetadata)
+                    .setStreamDuration(597 * 1000)
+                    .build();
+            MediaLoadOptions options = new MediaLoadOptions.Builder().setAutoplay(true).build();
+            final RemoteMediaClient remoteMediaClient = sessionManager.getCurrentCastSession().getRemoteMediaClient();
+            remoteMediaClient.registerCallback(new RemoteMediaClient.Callback() {
+                @Override
+                public void onStatusUpdated() {
+                    Intent intent = new Intent(MainActivity.this, ExpandedControlsActivity.class);
+                    startActivity(intent);
+                    remoteMediaClient.unregisterCallback(this);
+                }
+            });
+            remoteMediaClient.load(mediaInfo, options);
+        }
+
+        @Override
+        public void onSessionStartFailed(Session session, int i) {
+
+        }
+
+        @Override
+        public void onSessionEnding(Session session) {
+
+        }
+
+        @Override
+        public void onSessionEnded(Session session, int i) {
+
+        }
+
+        @Override
+        public void onSessionResuming(Session session, String s) {
+
+        }
+
+        @Override
+        public void onSessionResumed(Session session, boolean b) {
+            invalidateOptionsMenu();
+        }
+
+        @Override
+        public void onSessionResumeFailed(Session session, int i) {
+
+        }
+
+        @Override
+        public void onSessionSuspended(Session session, int i) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         castContext = CastContext.getSharedInstance(this);
+        sessionManager = castContext.getSessionManager();
 
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    protected void onResume() {
+        sessionManager.addSessionManagerListener(sessionManagerListener);
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sessionManager.removeSessionManagerListener(sessionManagerListener);
     }
 
     @Override public boolean onCreateOptionsMenu(Menu menu) {
--- js/main.js
+++ js/main.js
@@ -1,111 +1,35 @@
 var app = {
     init: function () {
-        cast.framework.CastReceiverContext.getInstance().start();
+        const context = cast.framework.CastReceiverContext.getInstance();
+        const playerManager = context.getPlayerManager();
 
-        document.addEventListener('keydown', function (event) {
-            switch(this.getKeyCode(event)) {
-                case app.KEY_LEFT:
-                    if (this.currentBtnIndex > 0)
-                        this.currentBtnIndex--;
-                    break;
-                case app.KEY_RIGHT:
-                    if (this.currentBtnIndex < 6)
-                        this.currentBtnIndex++;
-                    break;
-                case app.KEY_OK:
-                    switch(this.currentBtnIndex) {
-                        case 0:
-                            break;
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        case 3:
-                            break;
-                        case 4:
-                            break;
-                        case 5:
-                            break;
-                    }
-                    break;
-                default:
-                    break;
+        // intercept the LOAD request to be able to read in a contentId and get data
+        playerManager.setMessageInterceptor(
+            cast.framework.messages.MessageType.LOAD, loadRequestData => {
+                if (loadRequestData.media && loadRequestData.media.contentId) {
+                    console.log("contentId=" + loadRequestData.media.contentId);
+                    console.log("contentUrl=" + loadRequestData.media.contentUrl);
+                }
+                return loadRequestData;
             }
-            switch(this.currentBtnIndex) {
-                case 0:
-                    $('#btnPlay').focus();
-                    break;
-                case 1:
-                    $('#btnStop').focus();
-                    break;
-                case 2:
-                    $('#btnPause').focus();
-                    break;
-                case 3:
-                    $('#btnResume').focus();
-                    break;
-                case 4:
-                    $('#btnRewind').focus();
-                    break;
-                case 5:
-                    $('#btnForward').focus();
-                default:
-                    break;
-            }
-            event.preventDefault();
-            return true;
-        }.bind(this));
-        this.currentBtnIndex = 0;
-        $('#btnPlay').focus();
-    },
+        );
 
-    getKeyCode: function(event) {
-        switch (event.keyCode) {
-            case 40:
-                return app.KEY_DOWN;
-            case 37:
-                return app.KEY_LEFT;
-            case 39:
-                return app.KEY_RIGHT;
-            case 38:
-                return app.KEY_UP;
-            case 13:
-                return app.KEY_OK;
-            case 8:
-            case 27:
-            case 10009:
-                return app.KEY_BACK;
-            case 412:
-                return app.KEY_REWIND;
-            case 19:
-                return app.KEY_PAUSE;
-            case 417:
-                return app.KEY_FORWARD;
-            case 415:
-                return app.KEY_PLAY;
-            case 413:
-                return app.KEY_STOP;
-            default:
-                return app.KEY_UNKNOWN;
-        }
-    },
+        // listen to all Core Events
+        playerManager.addEventListener(cast.framework.events.category.CORE,
+            event => {
+              console.log(event);
+            }
+        );
+
+        const playbackConfig = new cast.framework.PlaybackConfig();
+        // Sets the player to start playback as soon as there are five seconds of
+        // media contents buffered. Default is 10.
+        playbackConfig.autoResumeDuration = 5;
+
+        context.start({playbackConfig: playbackConfig});
+    }
 };
 
 $(document).ready(function () {
-    app.KEY_UNKNOWN = -1;
-    app.KEY_OK = 0;
-    app.KEY_INFO = 1;
-    app.KEY_UP = 2;
-    app.KEY_DOWN = 3;
-    app.KEY_LEFT = 4;
-    app.KEY_RIGHT = 5;
-    app.KEY_REWIND = 6;
-    app.KEY_FORWARD = 7;
-    app.KEY_MENU = 8;
-    app.KEY_PLAY = 9;
-    app.KEY_PAUSE = 10;
-    app.KEY_BACK = 11;
-    app.KEY_STOP = 12;
-
     app.init();
 });
