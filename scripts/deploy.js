9828a8dc218b605474f2629e1a9f85292061c6a0
Justin Sanford <justin.h.sanford@gmail.com>
2018-08-14 15:21:47 -0400

    upload sample receiver app as well

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,12 +14,21 @@ if (environment === 'production') {
 let fullVersion = pkg.version;
 let majorVersion = fullVersion.split('.')[0];
 
-let file = fs.readFileSync('dist/chromecast-mux.js');
+let coreFile = fs.readFileSync('dist/chromecast-mux.js');
+let receiverHtml = fs.readFileSync('sample_app/receiver/index.html');
+let receiverJs = fs.readFileSync('sample_app/receiver/js/main.js');
+let receiverCss = fs.readFileSync('sample_app/receiver/css/style.css');
 let bucket = new AWS.S3({params: {Bucket: bucketName}});
 
 Promise.all([
-  uploadFile(bucket, file, fullVersion),
-  uploadFile(bucket, file, majorVersion)
+  uploadFile(bucket, coreFile, `chromecast/${fullVersion}/chromecast-mux.js`, 'application/javascript'),
+  uploadFile(bucket, coreFile, `chromecast/${majorVersion}/chromecast-mux.js`, 'application/javascript'),
+  uploadFile(bucket, receiverHtml, `chromecast/${fullVersion}/receiver/index.html`, 'text/html'),
+  uploadFile(bucket, receiverHtml, `chromecast/${majorVersion}/receiver/index.html`, 'text/html'),
+  uploadFile(bucket, receiverJs, `chromecast/${fullVersion}/receiver/js/main.js`, 'application/javascript'),
+  uploadFile(bucket, receiverJs, `chromecast/${majorVersion}/receiver/js/main.js`, 'application/javascript'),
+  uploadFile(bucket, receiverCss, `chromecast/${fullVersion}/receiver/css/style.css`, 'text/css'),
+  uploadFile(bucket, receiverCss, `chromecast/${majorVersion}/receiver/css/style.css`, 'text/css')
 ]).then((data) => {
   console.log(data);
   console.log('\n\nSuccess!');
@@ -28,13 +37,13 @@ Promise.all([
   process.exit(1);
 });
 
-function uploadFile (bucket, file, version) {
+function uploadFile (bucket, file, key, contentType) {
   return new Promise((resolve, reject) => {
     let params = {
       Body: file,
-      Key: `chromecast/${version}/chromecast-mux.js`,
+      Key: key,
       ACL: 'public-read',
-      ContentType: 'application/javascript'
+      ContentType: contentType
     };
 
     bucket.upload(params, function (err, data) {
