e4548275be6a0d62a17ad339de2406d3d2f97af1
stevenlyons <slyons@mux.com>
2020-01-20 14:23:30 -0800

    Change use of Date.now to perf timing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import mux from 'mux-embed';
 
 const log = mux.log;
 const assign = mux.utils.assign;
+const getTimestamp = mux.utils.getTimestamp;
 
 // Helper function to generate "unique" IDs for the player if your player does not have one built in
 const generateShortId = function () {
@@ -169,7 +170,7 @@ const monitorChromecastPlayer = function (player, options) {
         player.mux.emit('timeupdate');
         break;
       case cast.framework.events.EventType.SEGMENT_DOWNLOADED:
-        let now = Date.now();
+        let now = getTimestamp();
         let loadData = {
           request_event_type: 'SEGMENT_DOWNLOADED',
           request_start: now - event.downloadTime - 1,
