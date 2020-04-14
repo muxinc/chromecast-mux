0faac0e0eb80709d374b70558337751f1b142025
james-mux <james@mux.com>
2020-04-14 10:23:31 +0100

    always populate video_title, not just on videoChange

--- src/index.js
+++ src/index.js
@@ -65,7 +65,7 @@ const monitorChromecastPlayer = function (player, options) {
       player_is_paused: isPaused,
       video_source_width: videoSourceWidth,
       video_source_height: videoSourceHeight,
-
+      video_title: title,
       player_autoplay_on: autoplay,
       player_preload_on: undefined,
       video_source_url: mediaUrl,
