d91520055790a7a2d24be15a2cd89e805fccd87c
james-mux <james@mux.com>
2020-04-14 10:43:19 +0100

    testing automaticVideoChange

--- sample_app/receiver/js/main.js
+++ sample_app/receiver/js/main.js
@@ -5,9 +5,11 @@ var app = {
 
     initChromecastMux(playerManager, {
       debug: true,
-      data : {
+      automaticVideoChange: true,
+      data: {
         env_key: 'ikh9lsia6bh8pj5get2vnt6hm', // JW Testing
         player_init_time: Date.now(),
+        experiment_name: 'automaticVideoChange enabled'
       }
     });
 
